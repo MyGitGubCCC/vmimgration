e24024beba461015195c1b006004026ceea1542d
34448 <344486638@qq.com>
2020-04-04 20:32:33 +0800

    1、加入计算历史cpu平均值的方法

--- src/main/java/simulation/utils/ToArrayByFileReader.java
+++ src/main/java/simulation/utils/ToArrayByFileReader.java
@@ -5,6 +5,12 @@ import java.io.FileReader;
 import java.util.ArrayList;
 
 public class ToArrayByFileReader {
+
+    /**
+     * 这个是读取谷歌trace文件的方法
+     * @param name
+     * @return
+     */
     public static ArrayList<String[]> readerByFile(String name) {
         ArrayList<String> dataSet = new ArrayList<String>();
         try {
@@ -37,5 +43,47 @@ public class ToArrayByFileReader {
             arrayList.add(resource);
         }
         return arrayList;
+    }
+
+    /**
+     * 计算cpu历史利用率的平均值
+     * @param filename 历史利用率文件名
+     * @return
+     */
+    public static double calculateCpuHistoricalAverage(String filename){
+        ArrayList<String> dataSet = new ArrayList<String>();
+        try {
+            FileReader fr = new FileReader(filename);
+            BufferedReader bf = new BufferedReader(fr);
+            String str;
+            //按行读取字符串
+            while ((str = bf.readLine()) != null) {
+                dataSet.add(str);
+            }
+            bf.close();
+            fr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //对ArrayList中存储的字符串进行处理
+        int length = dataSet.size();
+        double avg = 0.0;
+
+        //9 10 11 12
+        for (int i = 0; i < length; i++) {
+            String s = dataSet.get(i);
+            double d = Double.parseDouble(s);
+            avg += d;
+        }
+        avg /= length;
+        return avg;
+
+    }
+
+
+    public static void main(String[] args) {
+        double avg = ToArrayByFileReader.calculateCpuHistoricalAverage(FilePath.DATA_PATH
+                + "\\Threeway-Migration\\cpuUtilization\\host_" + 0);
+        System.out.println(avg);
     }
 }
